bbd05f0bf425eed5d41def26ad4c5f19519e3f45
juan <ibifuhur.56>
2017-03-13 20:33:16 -0500

    configuración inicial del proyecto con la estructura de módulos en libGDX

---
+++ android/src/org/glud/ar/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.glud.ar;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.glud.ar.main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new main(), config);
+	}
+}
