58734238660e1e3f4e3c2dc429c9598a123b9441
juan <ibifuhur.56>
2017-03-17 16:08:16 -0500

    Cambiar a trasparencia

--- core/src/org/glud/ar/main.java
+++ core/src/org/glud/ar/main.java
@@ -18,7 +18,7 @@ public class main extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
